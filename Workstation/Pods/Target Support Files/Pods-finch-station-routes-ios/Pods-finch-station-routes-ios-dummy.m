f0eb04887945ed89c59ee7b6de8100878a9c256b
Joash Tubaga <joashtubaga@gmail.com>
2020-02-18 18:09:11 +0800

    [🎉] Initial Commit

---
+++ Workstation/Pods/Target Support Files/Pods-finch-station-routes-ios/Pods-finch-station-routes-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_finch_station_routes_ios : NSObject
+@end
+@implementation PodsDummy_Pods_finch_station_routes_ios
+@end
---
+++ Workstation/finch-station-routes-ios/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  finch-station-routes-ios
+//
+//  Created by Joash Tubaga on 2/18/20.
+//  Copyright © 2020 Joash Tubaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .white
+    }
+
+}
+
